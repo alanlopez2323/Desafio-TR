0738085c82065c08b48dff59c395655261707add
alanlopez2323 <filuxs23@gmail.com>
2025-07-01 23:46:06 -0300

    Create preguntas.js

---
+++ preguntas.js
@@ -0,0 +1,42 @@
+const preguntaSet = [
+  {
+    pregunta: "¿Cuál es la bebida nacional del Paraguay?",
+    opciones: ["Mate", "Guaraná", "Tereré", "Coca-Cola"],
+    correcta: "Tereré"
+  },
+  {
+    pregunta: "¿Qué idioma se habla junto al español en Paraguay?",
+    opciones: ["Quechua", "Mapuche", "Guaraní", "Portugués"],
+    correcta: "Guaraní"
+  },
+  {
+    pregunta: "¿Qué día se celebra la independencia de Paraguay?",
+    opciones: ["14 de julio", "15 de mayo", "12 de octubre", "3 de febrero"],
+    correcta: "15 de mayo"
+  },
+  {
+    pregunta: "¿Qué ingrediente NO es típico en un tereré tradicional?",
+    opciones: ["Cedrón", "Burrito", "Cocaína", "Menta"],
+    correcta: "Cocaína"
+  },
+  {
+    pregunta: "¿Qué personaje mitológico paraguayo protege la selva?",
+    opciones: ["Kurupí", "Pombero", "Jasy Jateré", "Karai Octubre"],
+    correcta: "Kurupí"
+  },
+  {
+    pregunta: "¿Qué comida es típica en Semana Santa en Paraguay?",
+    opciones: ["Pasta", "Chipa", "Hamburguesa", "Empanada"],
+    correcta: "Chipa"
+  },
+  {
+    pregunta: "¿Qué río pasa por Asunción?",
+    opciones: ["Río Paraná", "Río Paraguay", "Río Pilcomayo", "Río Tietê"],
+    correcta: "Río Paraguay"
+  },
+  {
+    pregunta: "¿Qué flor aparece en el billete de 100.000 Gs?",
+    opciones: ["Orquídea", "Lapacho", "Jazmín", "Rosa"],
+    correcta: "Lapacho"
+  }
+];
